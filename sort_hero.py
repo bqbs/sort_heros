9cc2376b318f3ff4914837cc36e877b9e1ef2eb2
lianjc <codemonkeyjc@gmail.com>
2019-08-22 17:32:40 +0800

    backup

--- sort_hero.py
+++ sort_hero.py
@@ -2,33 +2,32 @@
 import json
 import os
 import re
-import urllib.request
-from PIL import Image
-import colorsys
-import math
 import time
-from functools import cmp_to_key
-from sklearn.cluster import KMeans
-from collections import Counter
-import cv2  # for resizing image
+import urllib.request
+
+import math
+from PIL import Image
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
+from colormath.color_objects import sRGBColor, LabColor
 from colorthief import ColorThief
 
-from colormath.color_diff import delta_e_cie1976
+row_num = 12
+col_num = 12
 
 
-def merge_image(l):
+def merge_image(image_list):
     # 拼接头像
-    numPic = len(l)
-    print(numPic)
+    image_size = len(image_list)
+    print(image_size)
 
-    numrow = 12
-    numcol = 12  # 向下取整
-    toImage = Image.new('RGBA', (59 * numrow, 33 * numcol))  # 先生成头像集模板
+    # 先生成头像集模板
+    target_img = Image.new('RGBA', (59 * row_num, 33 * col_num))
 
-    x = 0  # 小头像拼接时的左上角横坐标
-    y = 0  # 小头像拼接时的左上角纵坐标
+    x = 0
+    y = 0
 
-    for index, i in enumerate(l):
+    for index, i in enumerate(image_list):
         try:
             # 打开图片
             img = Image.open(i)
@@ -38,14 +37,14 @@ def merge_image(l):
             # 缩小图片
             img = img.resize((59, 33), Image.ANTIALIAS)
             # 拼接图片
-            toImage.paste(img, (x * 59, y * 33))
+            target_img.paste(img, (x * 59, y * 33))
             x += 1
-            if x == numrow:
+            if x == row_num:
                 x = 0
                 y += 1
 
     print()
-    toImage.save('./sort_heros' + str(int(time.time())) + ".png")
+    target_img.save('./sort_heros' + str(int(time.time())) + ".png")
 
 
 def hex_to_rgb(value):
@@ -58,21 +57,18 @@ def rgb_to_hex(rgb):
     return '0x%02x%02x%02x%02x' % rgb
 
 
-def get_color_img(color_list):
-    numPic = len(color_list)
-    print(numPic)
+def gen_dominant_color_img(color_list):
+    color_size = len(color_list)
+    print(color_size)
 
-    numrow = 12
-    numcol = 12
-    toImage = Image.new('RGB', (59 * numrow, 33 * numcol))
+    target_image = Image.new('RGB', (59 * row_num, 33 * col_num))
 
     x = 0
     y = 0
 
-    for index, i in enumerate(color_list):
+    for index, color in enumerate(color_list):
         try:
-            lst = list(i)
-            lst.pop(len(lst) - 1)
+            lst = list(color)
             t = tuple(lst)
             img = Image.new('RGBA', (59, 33), t)
         except IOError:
@@ -81,157 +77,36 @@ def get_color_img(color_list):
             # 缩小图片
             img = img.resize((59, 33), Image.ANTIALIAS)
             # 拼接图片
-            toImage.paste(img, (x * 59, y * 33))
+            target_image.paste(img, (x * 59, y * 33))
             x += 1
-            if x == numrow:
+            if x == row_num:
                 x = 0
                 y += 1
 
-    toImage.save('./colors_' + str(int(time.time())) + ".png")
+    target_image.save('./colors_' + str(int(time.time())) + ".png")
 
 
-def compute_average_image_color(p):
-    img = Image.open(p)
-    img = img.convert('RGBA')
-    width, height = img.size
-
-    r_total = 0
-    g_total = 0
-    b_total = 0
-
-    count = 0
-    for x in range(0, width):
-        for y in range(0, height):
-            r, g, b, a = img.getpixel((x, y))
-            r_total += r
-            g_total += g
-            b_total += b
-            count += 1
-
-    return (int(r_total / count), int(g_total / count), int(b_total / count), a)
-
-
-def get_dominant_color(image_path):
-    # 颜色模式转换，以便输出rgb颜色值
-    image = Image.open(image_path)
-    image = image.convert('RGBA')
-
-    # 生成缩略图，减少计算量，减小cpu压力
-    image.thumbnail((200, 200))
-
-    max_score = 0
-    dominant_color = 0
-
-    for count, (r, g, b, a) in image.getcolors(image.size[0] * image.size[1]):
-        # 跳过纯黑色
-        if a == 0:
-            continue
-
-        saturation = colorsys.rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)[1]
-
-        y = min(abs(r * 2104 + g * 4130 + b * 802 + 4096 + 131072) >> 13, 235)
-
-        y = (y - 16.0) / (235 - 16)
-        #
-        # 忽略高亮色
-        if y > 0.9:
-            continue
-
-        # Calculate the score, preferring highly saturated colors.
-        # Add 0.1 to the saturation so we don't completely ignore grayscale
-        # colors by multiplying the count by zero, but still give them a low
-        # weight.
-        score = (saturation + 0.1) * count
-
-        if score > max_score:
-            max_score = score
-            dominant_color = (r, g, b, a)
-
-    return dominant_color
-
-
-def get_accent_color(path):
-    im = Image.open(path)
-    if im.mode != "RGB":
-        im = im.convert('RGB')
-    delta_h = 0.3
-    avg_h = sum(t[0] for t in
-                [colorsys.rgb_to_hsv(*im.getpixel((x, y))) for x in range(im.size[0]) for y in range(im.size[1])]) / (
-                im.size[0] * im.size[1])
-    beyond = filter(lambda x: abs(colorsys.rgb_to_hsv(*x)[0] - avg_h) > delta_h,
-                    [im.getpixel((x, y)) for x in range(im.size[0]) for y in range(im.size[1])])
-    if len(list(beyond)) > 0:
-        r = sum(e[0] for e in beyond) / len(list(beyond))
-        g = sum(e[1] for e in beyond) / len(list(beyond))
-        b = sum(e[2] for e in beyond) / len(list(beyond))
-        for i in range(im.size[0] / 2):
-            for j in range(im.size[1] / 10):
-                im.putpixel((i, j), (r, g, b))
-        im.save('res' + path)
-        return (r, g, b)
-    return (0, 0, 0)
-
-
-def get_dominant_color_new(image_path, k=4, image_processing_size=None):
-    """
-    takes an image as input
-    returns the dominant color of the image as a list
-
-    dominant color is found by running k means on the
-    pixels & returning the centroid of the largest cluster
-
-    processing time is sped up by working with a smaller image;
-    this resizing can be done with the image_processing_size param
-    which takes a tuple of image dims as input
-
-    >>> get_dominant_color(my_image, k=4, image_processing_size = (25, 25))
-    [56.2423442, 34.0834233, 70.1234123]
-    """
-    # resize image if new dims provided
-    image = cv2.imread(image_path)
-    # image = image.convert('RGBA')
-
-    if image_processing_size is not None:
-        image = cv2.resize(image, image_processing_size,
-                           interpolation=cv2.INTER_AREA)
-
-    # reshape the image to be a list of pixels
-    image = image.reshape((image.shape[0] * image.shape[1], 3))
-
-    # cluster and assign labels to the pixels
-    clt = KMeans(n_clusters=k)
-    labels = clt.fit_predict(image)
-
-    # count labels to find most popular
-    label_counts = Counter(labels)
-
-    # subset out most popular centroid
-    dominant_color = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
-
-    return list(dominant_color)[0]
-
-
-def get_dominant_color_3(image_path):
+def get_dominant_color_by_color_thief(image_path):
     color_thief = ColorThief(image_path)
     # get the dominant color
     dominant_color = color_thief.get_color(quality=6)
     l = list(dominant_color)
-
-    print(l)
-    l.append(0)
-    print(l)
-    print(tuple(l))
     return tuple(l)
 
 
 def get_all_hero_img():
+    """
+    获取英雄头像
+
+    :return:
+    """
     try:
         img_list = []
-        f = open('./sort_hero.json')
+        f = open('./dota2_heros.json')
         heros_json = json.load(f)
         for d in heros_json['data']:
             # print(d)
-            file_path = './images/%s' % re.search(r'[a-zA-Z\.\_]+png', d['img'], re.M | re.I).group()
+            file_path = './images/%s' % re.search(r'[a-zA-Z._]+png', d['img'], re.M | re.I).group()
             img_list.append(file_path)
             if not os.path.exists(file_path):
                 urllib.request.urlretrieve(d['img'], filename=file_path)
@@ -242,25 +117,17 @@ def get_all_hero_img():
         return []
 
 
-def color_sort(a):
-    try:
-        i = int(rgb_to_hex(a[1]), 16)
-        print(i)
-        return i
-
-    except Exception as e:
-        print(e)
+def colour_distance(rgb_1, rgb_2):
+    """
+    欧式距离计算方法
 
 
-def distance(c1, c2):
-    (r1, g1, b1, a1) = c1
-    (r2, g2, b2, a2) = c2
-    return math.sqrt((r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2)
-
-
-def ColourDistance(rgb_1, rgb_2):
-    R_1, G_1, B_1, A_1 = rgb_1
-    R_2, G_2, B_2, A_2 = rgb_2
+    :param rgb_1: rgb颜色值
+    :param rgb_2: rgb颜色值
+    :return:
+    """
+    R_1, G_1, B_1= rgb_1
+    R_2, G_2, B_2= rgb_2
     rmean = (R_1 + R_2) / 2
     R = R_1 - R_2
     G = G_1 - G_2
@@ -270,24 +137,29 @@ def ColourDistance(rgb_1, rgb_2):
 
 def sort_hero_by_color():
     img_list = get_all_hero_img()
-    color_list = []
+    obj_list = []
+    base_color = convert_color(sRGBColor(0, 0, 0), LabColor, target_illuminant='d50')
     for img in img_list:
-        t = get_dominant_color_3(img)
-        print(img, t)
-        # d = distance(t, (0, 0, 0, 0))
-        d = ColourDistance(t, (127, 127, 127, 127))
-        c = (img, t, d)
-        color_list.append(c)
+        dominant_color = get_dominant_color_by_color_thief(img)
 
-    color_list = sorted(color_list, key=lambda c: c[1])
+        delta_e = colour_distance(dominant_color, (0, 0, 0))
+
+        # rgb = sRGBColor(dominant_color[0], dominant_color[1], dominant_color[2])
+        # target_color = convert_color(rgb, LabColor, target_illuminant='d50')
+        # delta_e = delta_e_cie2000(target_color, base_color)
+
+        c = (img, dominant_color, delta_e)
+        obj_list.append(c)
+
+    obj_list = sorted(obj_list, key=lambda obj: obj[2])
     i_list = []
     c_list = []
-    for color in color_list:
+    for color in obj_list:
         i_list.append(color[0])
         c_list.append(color[1])
 
     merge_image(i_list)
-    get_color_img(c_list)
+    gen_dominant_color_img(c_list)
 
 
 if __name__ == '__main__':
